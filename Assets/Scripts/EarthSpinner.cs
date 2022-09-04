c098d12ccbcf1602d24df84d46b9dbbd728e86dd
NeilKleistGao <neilkleist@gmail.com>
2022-09-04 10:54:10 +0800

    Use rigidbody for rotation instead

--- Assets/Scripts/EarthSpinner.cs
+++ Assets/Scripts/EarthSpinner.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EarthSpinner : MonoBehaviour {
+    [SerializeField] private Rigidbody earthRigidBody;
+
     private Vector2 mScreenPosition = Vector2.zero;
 
     void Start() {
@@ -13,31 +15,35 @@ public class EarthSpinner : MonoBehaviour {
         if (Input.touchCount == 1) {
             var touch = Input.touches[0];
             if (touch.phase == TouchPhase.Began) {
+                Stop();
                 mScreenPosition = touch.position;
             }
             else if (touch.phase == TouchPhase.Moved) {
                 Rotate(touch.position);
             }
         }
-        else if (Input.touchCount == 2) { 
+        else if (Input.touchCount == 2) {
+            Stop();
             // TODO: scale
         }
 
         if (Input.GetMouseButtonDown(0)) {
-            mScreenPosition = Camera.main.WorldToScreenPoint(Input.mousePosition);
+            Stop();
+            mScreenPosition = Input.mousePosition;
         }
         else if (Input.GetMouseButton(0)) {
-            Rotate(Camera.main.WorldToScreenPoint(Input.mousePosition));
+            Rotate(Input.mousePosition);
         }
+    }
+
+    private void Stop() {
+        earthRigidBody.angularVelocity = Vector3.zero;
     }
 
     private void Rotate(Vector2 newPosition) {
         var sub = newPosition - mScreenPosition;
         mScreenPosition = newPosition;
 
-        if (sub.magnitude > 0.1f) {
-            var angle = transform.rotation.eulerAngles.y;
-            transform.rotation = Quaternion.AngleAxis(angle + sub.x * Time.deltaTime, Vector3.up);
-        }
+        earthRigidBody.AddForceAtPosition(sub * Time.deltaTime * 20.0f, Vector3.back);
     }
 }
